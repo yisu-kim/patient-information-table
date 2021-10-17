734ccc88f68b219f49a12ee55d2e700552802b24
yisu-kim <yisu.kim.dev@outlook.com>
2021-10-17 16:35:23 +0900

    Feat: [#11] Add filtering list utils for dummy data

---
+++ src/utils/dummy/genderList.ts
@@ -0,0 +1,7 @@
+import { GenderList } from 'utils/types/patient';
+
+export const generateGenderList = (): GenderList => {
+  return {
+    genderList: ['M', 'F'],
+  };
+};
---
+++ src/utils/dummy/raceList.ts
@@ -0,0 +1,7 @@
+import { RaceList } from 'utils/types/patient';
+
+export const generateRaceList = (): RaceList => {
+  return {
+    raceList: ['other', 'native', 'black', 'white', 'asian'],
+  };
+};
