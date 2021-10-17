0036e81218b7a29a621d101c4deddeb27a36f5b3
yisu-kim <yisu.kim.dev@outlook.com>
2021-10-17 17:29:28 +0900

    Feat: [#11] Update dummy data util for filtering

--- src/utils/dummy/patientList.ts
+++ src/utils/dummy/patientList.ts
@@ -1,5 +1,5 @@
 import { PatientListParams } from 'services/patient';
-import { PatientList } from 'utils/types/patient';
+import { Patient, PatientList } from 'utils/types/patient';
 
 export const generatePatientList = ({
   page = 1,
@@ -10,7 +10,7 @@ export const generatePatientList = ({
   const { order_column, gender, race, ethnicity, age_min, age_max, death } =
     rest;
   const totalLength = 1000;
-  const list = [];
+  let list: Patient[] = [];
 
   // for generating
   for (let i = (page - 1) * length + 1; i <= page * length; i++) {
@@ -41,6 +41,26 @@ export const generatePatientList = ({
         ? 1
         : 0;
     });
+  }
+
+  //for filtering
+  if (gender) {
+    list = list.filter((item) => item.gender === gender);
+  }
+  if (race) {
+    list = list.filter((item) => item.race === race);
+  }
+  if (ethnicity) {
+    list = list.filter((item) => item.ethnicity === ethnicity);
+  }
+  if (age_min) {
+    list = list.filter((item) => item.age >= age_min);
+  }
+  if (age_max) {
+    list = list.filter((item) => item.age <= age_max);
+  }
+  if (death) {
+    list = list.filter((item) => item.isDeath === death);
   }
 
   return {
