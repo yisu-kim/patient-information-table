72253a07ed9c97978a6d99564cd543da199745d6
yisu-kim <yisu.kim.dev@outlook.com>
2021-10-24 11:16:42 +0900

    Feat: [#19] Add sans-serif font

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,10 +6,17 @@ const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
   }
+  @font-face {
+    font-family: 'Noto Sans KR', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap')
+  }
   html,
   body,
   #root {
     height: 100%;
+    font-family: 'Noto Sans KR', sans-serif;
   }
   #root {
     display: flex;
